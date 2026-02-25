07e7a3164cabf6a0751e334c48c7895fff979408
Arush Handa <handaarush1237@gmail.com>
2026-02-25 15:42:55 -0500

    define what a checkin request should look like using pydantic

--- src/server/app.py
+++ src/server/app.py
@@ -6,3 +6,11 @@ from src.db.database import init_db, insert_log, get_all_logs, get_logs_by_bot, 
 app = FastAPI(title="C2 Server Simulation")
 
 init_db()
+
+#define what the data that the bots send should look like
+class CheckinRequest(BaseModel):
+    bot_id: str
+    source_ip: str = "127.0.0.1"
+    payload_size: int = 0
+    metadata: dict = {}
+
